29287702484538e83106ffd25fb06d58a02e5c95
Bannu <vidhyalakshmivaraprasadbathina@gmail.com>
2025-05-12 19:44:54 -0500

    Fetched title and description of articles from API

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import requests
 api_key = "f5ce3e24f5db42e69b8e411e784e675a"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2025-04-12&sortBy=publishedAt&apiKey=f5ce3e24f5db42e69b8e411e784e675a"
 
-request = requests.get(url)
-content = request.text
-print(content)
+request = requests.get(url)   #Make Request
+content = request.json()      #get a dictionary with data
+
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
