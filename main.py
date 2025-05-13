665163dd87c65bc07fb820abb1cac3c89064e13e
Bannu <vidhyalakshmivaraprasadbathina@gmail.com>
2025-05-12 20:26:10 -0500

    Send Email title and description of the new

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "f5ce3e24f5db42e69b8e411e784e675a"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2025-04-12&sortBy=publishedAt&apiKey=f5ce3e24f5db42e69b8e411e784e675a"
@@ -6,6 +7,12 @@ url = "https://newsapi.org/v2/everything?q=tesla&from=2025-04-12&sortBy=publishe
 request = requests.get(url)   #Make Request
 content = request.json()      #get a dictionary with data
 
+body = ""
+
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message = body)
+
